60da813e5a1971c10c32e5933128bcdeb77df708
FraCus1906 <c.francesca94@hotmail.it>
2026-01-28 10:04:01 +0100

    test fra

--- progetto/Login_Regi/LogReg.ts
+++ progetto/Login_Regi/LogReg.ts
@@ -20,6 +20,8 @@ class utente extends notlog_utente {
             this.pass = pass;
             this.email = email;
         }
+
+    //login nota scritta da fra da cancellare - ps ci sono cose da istallare da sul pc    
     async login(){
         console.log("===LOGIN===")
      let emailog:string = await input({message:"Inserisci l'email"}); 
