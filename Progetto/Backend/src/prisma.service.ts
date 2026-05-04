d41153399230b15b76141d805d44ab09cbe50b20
salvgit-afk <manninasalvatore434@gmail.com>
2026-05-04 18:48:22 +0200

    modificata installazione npm con pnpm in modo da essere allineato, nerl prisma.service aggiunto l'import di adapter che mancava per funzionare correttamente con la versione di prisma, nell'app.module aggiunti gli import di utente e nel tsconfig eliminati baseurl dato che è stato deprecato e l'ignore deprecations che non serve senza baseurl da ignorare

--- Progetto/Backend/src/prisma.service.ts
+++ Progetto/Backend/src/prisma.service.ts
@@ -1,16 +1,19 @@
+import 'dotenv/config';
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
-import * from '@prisma/client/default';
+import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
 
-constructor() {
-  super({} as any);
-}
+  constructor() {
+    const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+    super({ adapter });
+  }
 
   async onModuleInit() {
     await this.$connect();
-    console.log("✅ Database connesso correttamente!");
+    console.log("Database connesso correttamente");
   }
 
   async onModuleDestroy() {
