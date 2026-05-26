d25429845f9a620a3d508702fdc0e98dee07d297
Franko0z <francescomazzara0@gmail.com>
2026-05-26 21:58:39 +0200

    aggiunto auth tramite token jwt

--- Progetto/Backend/src/app.module.ts
+++ Progetto/Backend/src/app.module.ts
@@ -6,10 +6,25 @@ import { UtenteService } from './utente/utente.service';
 import { PrismaService } from './prisma.service';
 import { VeicoloController } from './veicolo/veicolo.controller';
 import { VeicoloService } from './veicolo/veicolo.service';
+import { JwtModule } from '@nestjs/jwt';          
+import { PassportModule } from '@nestjs/passport'; 
+import { JwtStrategy } from './utente/jwt.strategy'; 
 
 @Module({
-  imports: [],
+  imports: [
+    PassportModule,                               
+    JwtModule.register({                           
+      secret: process.env.JWT_SECRET, // generato con  node -e "console.log(require('crypto').randomBytes(64).toString('hex'))"
+      signOptions: { expiresIn: '1h' },
+    }),
+  ],
   controllers: [AppController, UtenteController, VeicoloController],
-  providers: [AppService, UtenteService, PrismaService, VeicoloService],
+  providers: [
+    AppService,
+    UtenteService,
+    PrismaService,
+    VeicoloService,
+    JwtStrategy,                                   
+  ],
 })
 export class AppModule {}
